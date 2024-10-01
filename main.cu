1346222ba06c8bc9d8d9162c666b54360f79041d
Michael Robson <mrobson@smith.edu>
2024-10-01 16:22:38 -0400

    main: Initial skeleton

---
+++ main.cu
@@ -0,0 +1,21 @@
+// include any headers
+
+// define constants
+
+// function to apply a 1d stencil to an array
+__global__ void stencil_1d(int *in, int *out) {
+}
+
+int main(void) {
+  // allocate input and output arrays
+
+  // initialize input array on the host
+
+  // run the kernel
+
+  // check for errors
+
+  // free memory
+
+  return 0;
+}
